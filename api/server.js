bc7f386bfa0f961496a3d008c2da43c0f1efd44b
Kalthom Musa <kalthommusa12345@gmail.com>
2024-11-27 09:50:47 +0300

    not render

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ import userRoutes from './routes/userRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
-import path from 'path';
+
 
 dotenv.config();
 
@@ -21,18 +21,13 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB is Connected!'))
 .catch(err => console.log(err));
 
-const __dirname = path.resolve();
 
 // Routes
 app.use("/api/user", userRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 // Error Handling Middleware
 app.use(errorHandler);
